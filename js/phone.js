a16fe444200911d4d6a8a1f945be4d20a57b60a2
Faizul-Hassan-2015 <86608620+Faizul-Hassan-2015@users.noreply.github.com>
2022-03-01 17:23:23 +0600

    add details button

--- js/phone.js
+++ js/phone.js
@@ -8,12 +8,48 @@ const searchMobile = () =>{
 
     fetch(url)
     .then(res => res.json())
-    .then(data => displaySearchResult(data.data))
+    .then(data => displaySearchResult(data.data.slice(0,20)))
 }
 
 const displaySearchResult = mobiles =>{
+    const searchResultDisplay = document.getElementById('search-display');
     // console.log(mobiles);
+
+    searchResultDisplay.innerHTML ='';
+
     mobiles.forEach(mobile =>{
-        console.log(mobile)
+        
+    
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML =`
+    <div class="card px-2">
+        <img src="${mobile.image}" class="card-img-top" height="400px" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${mobile.phone_name}</h5>
+            <p class="card-text">${mobile.brand}</p>
+            </div>
+        <button onclick="loadMobileDetails('${mobile.slug}')" class="w-25 ms-2 mb-2 text-white rounded  bg-primary border-0">Details</button>
+    </div>
+    `;
+    searchResultDisplay.appendChild(div)
+        // console.log(mobile)
     })
+}
+
+const loadMobileDetails = mobileId =>{
+    const url =`https://openapi.programming-hero.com/api/phone/${mobileId}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMobileDetails(data.data))
+}
+
+const displayMobileDetails = mobile =>{
+    console.log(mobile)
+    const displayDetails = document.getElementById('display-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    
+    `;
 }
