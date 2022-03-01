82249e990d9c709ff201460f4a08ff193504ff02
Faizul-Hassan-2015 <86608620+Faizul-Hassan-2015@users.noreply.github.com>
2022-03-01 20:30:27 +0600

    add error message

--- js/phone.js
+++ js/phone.js
@@ -1,17 +1,19 @@
+// search field function 
 const searchMobile = () =>{
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
 
     searchField.value = '';
 
-    if( searchText === ''){
+    // alert message for empty string search 
+    const error = document.getElementById('error');
+    if( searchText === '' ){
+        error.style.display ='block';
     const searchResultDisplay = document.getElementById('search-display');
     const displayDetails = document.getElementById('display-details');
     displayDetails.innerHTML = '';
     searchResultDisplay.innerHTML ='';
 
-    alert("Please enter a valid mobile name!!!");
-    return false;
     } 
 
     else{
@@ -20,11 +22,12 @@ const searchMobile = () =>{
         fetch(url)
         .then(res => res.json())
         .then(data => displaySearchResult(data.data.slice(0,20)))
+        error.style.display ='none';
     }
 
     
 }
-
+// display search result 
 const displaySearchResult = mobiles =>{
     const searchResultDisplay = document.getElementById('search-display');
     const displayDetails = document.getElementById('display-details');
@@ -47,10 +50,9 @@ const displaySearchResult = mobiles =>{
     </div>
     `;
     searchResultDisplay.appendChild(div)
-        // console.log(mobile)
     })
 }
-
+// mobile details function 
 const loadMobileDetails = mobileId =>{
     const url =`https://openapi.programming-hero.com/api/phone/${mobileId}`;
     fetch(url)
@@ -58,26 +60,33 @@ const loadMobileDetails = mobileId =>{
     .then(data => displayMobileDetails(data.data))
 }
 
+// display mobile details data 
 const displayMobileDetails = mobile =>{
-    console.log(mobile)
     const displayDetails = document.getElementById('display-details');
     displayDetails.innerHTML = '';
         const div = document.createElement('div');
         div.classList.add('card');
         div.innerHTML = `
         <img height="400px" src="${mobile.image}" class="card-img-top p-3" alt="...">
-                <div class="card-body ">
-                  <h5 class="name">Name : ${mobile.name}</h5>
-                  <h6 class="text"> Main Features </h6>
-                  <p>Storage : ${mobile.mainFeatures.storage}</p>
-                  <p>Release Date : ${mobile.releaseDate}</p>
-                  <p>Sensors : ${mobile.mainFeatures.sensors} </p>
+            <div class="card-body ">
+                <h5 class="name">Name : ${mobile.name}</h5>
+                <h6 class="text"> Main Features </h6>
+                  <p><span class="dark">Storage</span> : ${mobile.mainFeatures.storage}</p>
+                  <p><span class="dark">Sensors</span> : ${mobile.mainFeatures.sensors} </p>
+                  <p><span class="dark">ChipSet</span> : ${mobile.mainFeatures.chipSet} </p>
+                  <p><span class="dark">Display Size</span> : ${mobile.mainFeatures.displaySize} </p>
+                  <p><span class="dark">Release Date</span> : ${mobile.releaseDate ? mobile.releaseDate : "No release-date found!!"}</p>
                 <h6 class="text"> Other Features </h6>
-                  <p>Bluetooth : ${mobile.others.Bluetooth}</p>
-                  <p>Radio : ${mobile.others.Radio}</p>
+                  <p><span class="dark">Bluetooth</span> : ${mobile.others.Bluetooth}</p>
+                  <p><span class="dark">Radio</span> : ${mobile.others.Radio}</p>
+                  <p><span class="dark">GPS</span> : ${mobile.others.GPS}</p>
+                  <p><span class="dark">NFC</span> : ${mobile.others.NFC}</p>
+                  <p><span class="dark">USB</span> : ${mobile.others.USB}</p>
+                  <p><span class="dark">WLAN</span> : ${mobile.others.WLAN}</p>
                   
-                </div>
+            </div>
         `;
+        // loop for sensors 
         mobile.mainFeatures.sensors.forEach(sensor => {
             const sensors = sensor;
             })
